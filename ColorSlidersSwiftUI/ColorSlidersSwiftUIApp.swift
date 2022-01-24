0773d16440eb4ce21b431ea14ba5f2392c357b60
Andrey Khakimov <khakimov013@gmail.com>
2022-01-24 13:30:32 +0300

    Initial Commit

---
+++ ColorSlidersSwiftUI/ColorSlidersSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSlidersSwiftUIApp.swift
+//  ColorSlidersSwiftUI
+//
+//  Created by Andrey Khakimov on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorSlidersSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
