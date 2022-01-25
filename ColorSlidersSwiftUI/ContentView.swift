cbd4745d2c92ab190e1b7ee17a37d26e4234734c
Andrey Khakimov <khakimov013@gmail.com>
2022-01-25 23:42:16 +0300

    Added ColorSliderView with TF

--- ColorSlidersSwiftUI/ContentView.swift
+++ ColorSlidersSwiftUI/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var redColorValue: Double = 70
+    @State private var greenColorValue: Double = 160
+    @State private var blueColorValue: Double = 220
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 20) {
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 350, height: 150)
+                .padding()
+                .foregroundColor(
+                    Color(red: redColorValue / 255,
+                          green: greenColorValue / 255,
+                          blue: blueColorValue / 255)
+                )
+            ColorSliderView(value: $redColorValue, color: .red)
+            ColorSliderView(value: $greenColorValue, color: .green)
+            ColorSliderView(value: $blueColorValue, color: .blue)
+            Spacer()
+        }
     }
 }
 
@@ -19,3 +35,34 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct ColorSliderView: View {
+    @Binding var value: Double
+    let color: Color
+    
+    var body: some View {
+        HStack {
+            Text("\(value.formatted(.number))").foregroundColor(color)
+                .frame(width: 40)
+            Slider(value: $value, in: 0...255, step: 1)
+                .tint(color)
+            TextField("", value: $value, format: .number)
+                .frame(width: 40)
+            
+        }
+        .padding()
+    }
+}
+
+extension ContentView {
+    func checkTextField(text: String) -> Bool {
+        if text.count > 3 {
+            return false
+        }
+        if let value = Double(text), value >= 0 {
+            return true
+        }
+        return false
+    }
+}
+
